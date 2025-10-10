723f5d14f405ee1f6ca32b06282a5d1f55cfc305
Barry O'Neill <barra.oneill@gmail.com>
2025-10-09 23:59:33 -0400

    upgrades

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -7,22 +7,22 @@ object Dependencies {
   val TestLib = Seq(
     testFrameworks += new TestFramework("munit.Framework"),
     libraryDependencies ++= Seq(
-      "org.scalameta" %% "munit"         % "1.1.0",
-      "org.mockito"    % "mockito-core"  % "5.16.0",
-      "org.gnieh"     %% "diffson-circe" % "4.6.0"
+      "org.scalameta" %% "munit"         % "1.2.0",
+      "org.mockito"    % "mockito-core"  % "5.20.0",
+      "org.gnieh"     %% "diffson-circe" % "4.6.1"
     ).map(_ % Test)
   )
 
-  val Slack = libraryDependencies += "com.slack.api" % "slack-app-backend" % "1.45.3"
+  val Slack = libraryDependencies += "com.slack.api" % "slack-app-backend" % "1.45.4"
 
   val Refined = libraryDependencies += "eu.timepit" %% "refined" % "0.11.3"
 
   val NewTypes = libraryDependencies += "io.monix" %% "newtypes-core" % "0.3.0"
 
   val Logging = libraryDependencies ++= Seq(
-    "org.typelevel" %% "log4cats-slf4j"  % "2.7.0",
-    "ch.qos.logback" % "logback-classic" % "1.5.17" % Test,
-    "ch.qos.logback" % "logback-core"    % "1.5.17" % Test,
+    "org.typelevel" %% "log4cats-slf4j"  % "2.7.1",
+    "ch.qos.logback" % "logback-classic" % "1.5.19" % Test,
+    "ch.qos.logback" % "logback-core"    % "1.5.19" % Test,
     "org.slf4j"      % "jcl-over-slf4j"  % "2.0.17" % Test,
     "org.slf4j"      % "jul-to-slf4j"    % "2.0.17" % Test
   )
@@ -32,10 +32,10 @@ object Dependencies {
     "http4s-ember-server",
     "http4s-ember-client",
     "http4s-circe"
-  ).map("org.http4s" %% _ % "0.23.30")
+  ).map("org.http4s" %% _ % "0.23.32")
 
-  val CirceVersion = "0.14.10"
-  val Circe = libraryDependencies ++= Seq(
+  val CirceVersion = "0.14.15"
+  val Circe        = libraryDependencies ++= Seq(
     "io.circe" %% "circe-core"   % CirceVersion,
     "io.circe" %% "circe-parser" % CirceVersion
   )
